f42787d7e034ed43c4ffc6a4e43fd0376e455237
Keertikeya Gupta <keertikeya.gupta@gmail.com>
2025-05-15 22:50:16 -0500

    Get top100
    * Adding code to get top 100 players in ongoing season in the entire league.

--- libs/Bowler.py
+++ libs/Bowler.py
@@ -4,35 +4,39 @@ def getNumberOfBalls(overs):
 def getEconomyRate(runs, overs):
     return round(runs*6/getNumberOfBalls(overs), 2)
 
-
 def getBowlingAverage(runs, wickets):
-    return round(runs/wickets, 2)
+    try:
+        return round(runs/wickets, 2)
+    except ZeroDivisionError:
+        return None
 
 def getBowlingStrikeRate(overs, wickets):
-    return round(getNumberOfBalls(overs)/wickets, 2)
+    try:
+        return round(getNumberOfBalls(overs)/wickets, 2)
+    except ZeroDivisionError:
+        return None
 
 
 class Bowler:
-    def __init__(self, name, teamName, innings, overs, runs, wickets):
+    def __init__(self, name, teamName, innings, overs, maidens, runs, wickets):
         self.name = name
         self.teamName = teamName
-        self.innings = innings
-        self.overs = overs
-        self.runs = runs
-        self.wickets = wickets
+        self.innings = int(innings)
+        self.overs = float(overs)
+        self.runs = int(runs)
+        self.wickets = int(wickets)
 
         self.econ = getEconomyRate(self.runs, self.overs)
         self.avg = getBowlingAverage(self.runs, self.wickets)
         self.str = getBowlingStrikeRate(self.overs, self.wickets)
 
-
-    def __str__(self):
-        return (f"{self.name},"
-                f"{self.teamName},"
-                f"{self.runs},"
-                f"{self.wickets},"
-                f"{self.overs},"
-                f"{self.econ},"
-                f"{self.avg},"
-                f"{self.str}"
-        )
+    # def __str__(self):
+    #     return (f"{self.name},"
+    #             f"{self.teamName},"
+    #             f"{self.runs},"
+    #             f"{self.wickets},"
+    #             f"{self.overs},"
+    #             f"{self.econ},"
+    #             f"{self.avg},"
+    #             f"{self.str}"
+    #     )
--- main.py
+++ main.py
@@ -1,4 +1,7 @@
 from bs4 import BeautifulSoup as bs
+
+from libs.Batsman import Batsman
+from libs.Bowler import Bowler
 from libs.Stats import BattingStats as bats
 from libs.Stats import BattingStats as bowls
 # from libs.DivisionInfo import
@@ -6,6 +9,8 @@ from libs.Stats import BattingStats as bowls
 import requests
 import argparse
 import logging
+import time
+import heapq
 
 
 logging.basicConfig(level=logging.INFO)
@@ -32,7 +37,123 @@ def getPlayerDetails(player):
     pass
 
 def getTop100():
-        pass
+    logging.info("Getting top 100 batsmen and bowlers for current season")
+    divisionLinks = []
+
+    allBatsmen = []
+    allBowlers = []
+
+    # Getting homepage HTML content
+    homePage = requests.get(url)
+    logging.debug(f"Home retrieved:\n{homePage.text}")
+    soup = bs(homePage.content, "html.parser")
+    logging.debug(f"Soup has been made:\n {soup.text}")
+
+    # Getting current season info
+    seasonsPOS = soup.find("div", attrs={"id": "seasonspos"})
+    currentSeason = seasonsPOS.find_all("option")[0].text.strip()
+    logging.info(f"Season name: {currentSeason}")
+
+    # Getting all division links
+    poolHeaderMenu = soup.find("div", attrs={"id": "poolheadermenu"})
+    for division in poolHeaderMenu.find_all("li"):
+        if division.text.strip().startswith("Div"):
+            divisionLinks.append(division.find("a").get("href"))
+
+    logging.info("All division links have been retrieved.")
+    logging.debug(f"All men's division links:\n{divisionLinks}")
+
+    # Check if current season is invalid, i.e. doesn't have any data
+    divLink = requests.get(url + divisionLinks[0])
+    div0Soup = bs(divLink.content, "html.parser")
+    if len(div0Soup.find_all("table")) == 0:
+        currentSeasonId = int(divisionLinks[0].split("season_id=")[1])
+        logging.info(f"Current season (id = {currentSeasonId}) has either not started yet or is invalid")
+        logging.info(f"Getting info for previous season instead (season_id={currentSeasonId - 1}).")
+        for i in range(len(divisionLinks)):
+            divisionLinks[i] = divisionLinks[i].replace(str(currentSeasonId), str(currentSeasonId - 1))
+
+
+    for divisionLink in divisionLinks:
+        # 1. Go to each division's page
+        logging.info(f"Getting division details: {url + divisionLink}")
+        divPage = requests.get(url + divisionLink)
+        divSoup = bs(divPage.content, "html.parser")
+        try:
+            teamsTable = divSoup.find_all("table")[0]
+        except IndexError:
+            logging.info("Division table has not been retrieved.")
+            continue
+
+        teamRows = teamsTable.find_all("tr")[1:]
+
+        teamLinks = []
+        for teamRow in teamRows:
+            teamLinks.append(teamRow.find_all("td")[0].find("a").get("href"))
+
+        # 2. Go to each team's page in the division
+        for teamLink in teamLinks:
+            teamPage = requests.get(url + "/Pages/UI/" + teamLink)
+            teamSoup = bs(teamPage.content, "html.parser")
+
+            # Get batting stats for team
+            battingTable = teamSoup.find_all("table")[2]
+            logging.debug(f"Batting table retrieved:\n{battingTable.text}")
+
+            batsmanRows = battingTable.find_all("tr")[1:]
+            logging.info(f"SETTING BATSMAN DETAILS FOR TEAM: {batsmanRows[0].find_all("td")[2].text}")
+            for batsmanRow in batsmanRows:
+                batsman = Batsman(
+                    name=batsmanRow.find_all("td")[0].text.strip(),
+                    teamName=batsmanRow.find_all("td")[2].text.strip(),
+                    innings=batsmanRow.find_all("td")[3].text.strip(),
+                    runs=batsmanRow.find_all("td")[4].text.strip(),
+                    balls=batsmanRow.find_all("td")[5].text.strip(),
+                    fours=batsmanRow.find_all("td")[6].text.strip(),
+                    sixes=batsmanRow.find_all("td")[7].text.strip()
+                )
+                batsman.setBattingAverage()
+                batsman.setStrikeRate()
+
+                allBatsmen.append(batsman)
+
+            # Get bowling stats for team
+            bowlingTable = teamSoup.find_all("table")[3]
+            logging.debug(f"Bowling table retrieved:\n{bowlingTable.text}")
+
+            bowlerRows = bowlingTable.find_all("tr")[1:]
+            for bowlerRow in bowlerRows:
+                bowler = Bowler(
+                    name=bowlerRow.find_all("td")[0].text.strip(),
+                    teamName=bowlerRow.find_all("td")[2].text.strip(),
+                    innings=bowlerRow.find_all("td")[3].text.strip(),
+                    overs=bowlerRow.find_all("td")[4].text.strip(),
+                    maidens=bowlerRow.find_all("td")[5].text.strip(),
+                    runs=bowlerRow.find_all("td")[6].text.strip(),
+                    wickets=bowlerRow.find_all("td")[7].text.strip()
+                )
+
+                allBowlers.append(bowler)
+
+
+    allBatsmen.sort(key=lambda b: b.runs, reverse=True)
+    allBowlers.sort(key=lambda b: b.wickets, reverse=True)
+
+    logging.info(f"{len(allBatsmen)} batsmen info has been added.")
+    logging.info(f"{len(allBowlers)} bowlers info has been added.")
+
+    logging.info("Top 100 batsmen:")
+    for i in range(100):
+        logging.info(f"{allBatsmen[i].name}, {allBatsmen[i].teamName}, {allBatsmen[i].runs}")
+
+    logging.info("Top 100 bowlers:")
+    for i in range(100):
+        logging.info(f"{allBowlers[i].name}, {allBowlers[i].teamName}, {allBowlers[i].wickets}")
+
+
+    # 3. Retrieve player information for each team
+    pass
+
 
 
 
@@ -44,4 +165,8 @@ elif args.player:
     getPlayerDetails(args.player)
 else:
     # Get top 100 players (100 bowlers and 100 batsmen) in the entire league
+    start = int(time.time())
     getTop100()
+    end = int(time.time())
+
+    logging.info(f"Time elapsed: {end - start} seconds")
